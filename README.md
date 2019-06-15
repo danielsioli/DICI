840afe8d2f717dabb4fce37b4b2c13c67f9ef98b
Daniel da Silva Oliveira <47244844+danielsioli@users.noreply.github.com>
2019-06-15 13:49:07 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# pyDICI
+Pacote para enviar dados ao DICI
+
+## Install 
+
+```
+pip install pyDICI
+```
+
+## Use
+Importar pacote
+```
+import pyDICI as dici
+```
+Enviar Arquivo
+```
+dici.enviar('Resoluções Anatel','Resoluções Citações','p','C:\arquivo.csv')
+```
+Olhar o Help
+```
+dici.help()
+```
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='pyDICI',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/danielsioli/DICI',
+    author='Daniel Oliveira',
+    author_email='danielsioli@gmail.com',
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    description='Pacote para enviar dados ao DICI',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='anatel,dici',
+    install_requires=['selenium','argparse','pandas','termcolor'],
+    include_package_data=True,
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
